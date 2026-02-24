7d2b2f9404f70112ccdc157e6d377fe4113bf3a7
Rodri <rodrigoguidoarias@gmail.com>
2026-02-24 21:31:49 +0100

    Feat: module 01 - add apply_discount and __str__ to Book

--- module_01_fundamentals/project/book.py
+++ module_01_fundamentals/project/book.py
@@ -3,3 +3,14 @@ class Book:
         self.title = title
         self.author = author
         self.price = price
+
+    def apply_discount(self,percent):
+        if(percent>=100):
+            self.price = 0
+        else:
+            self.price = self.price * (1 - percent/100)
+        return self.price
+        
+    
+    def __str__(self):
+        return (f"{self.title} by {self.author} - {self.price}€")
--- module_01_fundamentals/tests/test_book.py
+++ module_01_fundamentals/tests/test_book.py
@@ -1,19 +1,38 @@
 from module_01_fundamentals.project.book import Book
 
 # Test  1 . CRear un libro correctamente
-def test_create_book():
-    book = Book("The Great Gatsby", "F. Scott Fitzgerald", 10.99)
-    assert book.title == "The Great Gatsby"
-    assert book.author == "F. Scott Fitzgerald"
-    assert book.price == 10.99
+def test_book_creation():
+    book = Book("Clean Code", "Robert Martin", 35.0)
+    assert book.title == "Clean Code"
+    assert book.author == "Robert Martin"
+    assert book.price == 35.0
 
 # Test 2. El titulo no puede estar vacio
 def test_book_title_not_empty():
-    book = Book("The Great Gatsby", "F. Scott Fitzgerald", 10.99)
+    book = Book("El Quijote", "Cervantes", 20.0)
     assert book.title != ""
     
 # Test 3. El precio no puede ser negativo
-def test_book_price_not_negative():
-    book = Book("The Great Gatsby", "F. Scott Fitzgerald", 10.99)
-    assert book.price >= 0
+def test_book_price_is_positive():
+    book = Book("Python Crash Course", "Eric Matthes", 29.99)
+    assert book.price > 0
+        
+# Test 4. Aplicar un descuento al precio del libro
+
+def test_book_apply_discount():
+    book = Book("Clean Code","Robert Martin",40.0)
+    result = book.apply_discount(25)
+    assert result == 30.0
     
+# Test 5. El descuento no puede ser mayor al 100%
+
+def test_discount_canoot_exceed_100():
+    book = Book("Clean Code","Robert Martin",40)
+    result = book.apply_discount(110)
+    assert result == 0.0 
+
+# Test 6. Representación en texto del libro
+
+def test_book_str_representation():
+    book = Book("Clean Code", "Robert Martin", 35.0)
+    assert str(book) == "Clean Code by Robert Martin - 35.0€"
